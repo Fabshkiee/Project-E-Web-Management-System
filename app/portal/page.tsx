d65bdc70d0669c42dd2398b656287fa417461f4d
Fabshkiee <pwedefabros@gmail.com>
2026-03-23 22:22:35 +0800

    feat: greet user section

--- app/portal/page.tsx
+++ app/portal/page.tsx
@@ -1,8 +1,28 @@
+import { QRCodeSVG } from "qrcode.react";
+
 export default function Portal() {
   return (
     <main>
-      <h1>Portal</h1>
-      <p>Welcome to your personal fitness portal.</p>
+      {/**Exit Button */}
+      <div>
+        <button>Exit</button>
+      </div>
+      {/**Greet User Section */}
+      <div className="flex justify-center flex-col items-center gap-2">
+        <h1 className="h3-b-lexend">Welcome, Alex</h1>
+        <p className="p-sm-md text-muted">Ready to train today?</p>
+      </div>
+
+      {/**QR Code Section */}
+      <section className="flex justify-center flex-col items-center">
+        <QRCodeSVG
+          value="https://google.com"
+          size={200}
+          level={"M"}
+          marginSize={1}
+          className="rounded-xl"
+        />
+      </section>
     </main>
   );
 }
