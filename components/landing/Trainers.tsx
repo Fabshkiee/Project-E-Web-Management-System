ea8e2d73070e031dfd6ca3a79b63760615c00aca
Kennedyyy67 <earlnengasca123@gmail.com>
2026-03-28 12:12:17 +0800

    Fix changes for merging

--- components/landing/Trainers.tsx
+++ components/landing/Trainers.tsx
@@ -1,84 +1,63 @@
-import Image from 'next/image';
-
-
+import Image from "next/image";
 
 export default function Trainers() {
   return (
-
-    <section className="py-27 pt-5 px-5 border-b bg-land-dark">
-
-
-    {/* Title Bar Container */}
-      <div className="container px-4 text-left mt-10">
-        <h2 className="landing-h1 text-5xl">Meet Your Trainers</h2>
-        <p className="landing-p-lg mt-8 text-gray-300">
-        Elite coaches dedicated to your performance.
+    <section className="py-[80px]  bg-land-dark">
+      {/* Title Bar Container */}
+      <div className="container px-[32px] text-left">
+        <h2 className="landing-h2">Meet Your Trainers</h2>
+        <p className="landing-p-lg mt-[16px] text-muted">
+          Elite coaches dedicated to your performance.
         </p>
       </div>
 
-
-    {/* Trainers Containers */}
-      <div className="container px-10 md:px-17 xl:px-32 mt-25 grid grid-cols-1 md:grid-cols-2 gap-15 place-items-center mx-auto">
-
-        
-        
-        {/* 1st Trainer Card */}
-        <div className=" relative group overflow-hidden bg-land-light rounded-md text-center ring-3 ring-land-crimson/20 shadow-2xl shadow-black">
+      {/* Trainers Containers */}
+      <div
+        className="container px-10 md:px-17 xl:px-[214px] mt-[48px] grid grid-cols-1 md:grid-cols-2 gap-[200px] place-items-center mx-auto py-[25px]
+      "
+      >
+        {/* 2nd Trainer Card */}
+        <div className=" relative group overflow-hidden bg-land-light rounded-md text-center ring-3 ring-land-crimson/20 shadow-2xl shadow-black w-[369.6px] h-[545.6px] ">
           <img
-            src="/assets/coachEric.svg"
+            src="/assets/coachEric.webp"
             alt="Coach Eric"
-            className="w-full h-[610.6px] rounded-md mx-auto object-cover"
+            className="w-full h-full  rounded-md object-cover"
           />
 
-        <div className="absolute inset-0 bg-gradient-to-t from-land-dark via-land-dark/50 to-transparent"></div>
+          <div className="absolute inset-0 bg-gradient-to-t from-black via-land-black/50 to-transparent"></div>
 
-        {/* Text Container */}
-        <div className="absolute bottom-10 left-10 text-left">
-
-          {/* Name of Coach */}
-          <div className=" text-4xl md:text-5xl font-semibold font-teko mb-5">
-           ERIC DIONES
-          </div>
-          {/* Coach Specialty */}
-          <div className="landing-h4 font-light  text-base md:text-lg text-land-crimson font-space tracking-widest">
-          OWNER/SPECIALTY
+          {/* Text Container */}
+          <div className="absolute bottom-[35.2px] left-[35.2px] right-[35.2px] text-left">
+            {/* Name of Coach */}
+            <div className=" h3-b">ERIC DIONES</div>
+            {/* Coach Specialty */}
+            <div className="landing-p-sm tracking-[1.4px] mt-[8px] text-land-crimson ">
+              OWNER/SPECIALTY
+            </div>
           </div>
         </div>
 
-      </div>
-
-
-
-      {/* 2nd Trainer Card */}
-        <div className=" relative group overflow-hidden bg-land-light rounded-md text-center ring-3 ring-land-crimson/20 shadow-2xl shadow-black ">
+        {/* 2nd Trainer Card */}
+        <div className=" relative group overflow-hidden bg-land-light rounded-md text-center ring-3 ring-land-crimson/20 shadow-2xl shadow-black w-[369.6px] h-[545.6px] ">
           <img
-            src="/assets/coachEzekiel.svg"
+            src="/assets/coachEzekiel.webp"
             alt="Coach Ezekiel"
-            className="w-full h-[610.6px] rounded-md mx-auto object-cover"
+            className="w-full h-full  rounded-md object-cover"
           />
 
-        <div className="absolute inset-0 bg-gradient-to-t from-land-dark via-land-dark/50 to-transparent"></div>
+          <div className="absolute inset-0 bg-gradient-to-t from-black via-land-black/50 to-transparent"></div>
 
-        {/* Text Container */}
-        <div className="absolute bottom-10 left-10 text-left">
-
-          {/* Name of Coach */}
-          <div className=" text-4xl md:text-5xl font-semibold font-teko mb-5">
-           COACH EZEKIEL
-          </div>
-          {/* Coach Specialty */}
-          <div className="landing-h4 font-light  text-base md:text-lg text-land-crimson font-space tracking-widest">
-          SPECIALTY
+          {/* Text Container */}
+          <div className="absolute bottom-[35.2px] left-[35.2px] right-[35.2px] text-left">
+            {/* Name of Coach */}
+            <div className=" h3-b">COACH EZEKIEL</div>
+            {/* Coach Specialty */}
+            <div className="landing-p-sm tracking-[1.4px] mt-[8px] text-land-crimson ">
+              SPECIALTY
+            </div>
           </div>
         </div>
-
-
       </div>
-
-
-    </div>
-
-
     </section>
   );
 }
