dc7b24d98f29f106ba87648e868e9b57be18d233
Kennedyyy67 <earlnengasca123@gmail.com>
2026-04-03 23:39:19 +0800

    Updated Membership Plans Section

--- components/landing/Membership.tsx
+++ components/landing/Membership.tsx
@@ -1,3 +1,58 @@
+const MEMBERSHIP_PLANS = [
+  {
+    id: "dayPass1",
+    title: "Day Pass",
+    price: "150",
+    unit: "day",
+    description: "Perfect for first-timers or travelers.",
+    features: [
+      "Access to strength equipment",
+      "Standard shower & locker use",
+      "Valid for 24 hours",
+    ],
+  },
+  {
+    id: "dayPass2",
+    title: "Student Day Pass",
+    price: "100",
+    unit: "day",
+    description: "A budget-friendly option for students.",
+    features: [
+      "Accesss to strength equipment",
+      "Standard shower & locker use",
+      "Valid for 24 hours",
+      "Save 33%",
+    ],
+  },
+  {
+    id: "memPlan1",
+    title: "Monthly Pass",
+    price: "900",
+    unit: "monthly",
+    description: "The complete fitness experience.",
+    features: [
+      "Unlimited Entry and Access",
+      "Full amenity access",
+      "Valid for 30 days",
+    ],
+  },
+  {
+    id: "memPlan2",
+    title: "Student Monthly Pass",
+    price: "700",
+    unit: "monthly",
+    description:
+      "Best option for student atheletes. Valid student ID required upon registration.",
+    features: [
+      "Unlimited Entry and Access",
+      "Full amenity access",
+      "Save 22% off from standard monthly rate.",
+      "Valid for 30 days",
+    ],
+    isPopular: true,
+  },
+];
+
 export default function Membership() {
   return (
     <section className=" relative z-0 overflow-hidden">
@@ -5,8 +60,8 @@ export default function Membership() {
       <div className="relative isolate  ">
         {/* THE BLOB OF RED BEHIND THE TEXT */}
         <div
-          className="absolute inset-0 bg-land-crimson/5 rounded-[9999px] w-[800px] h-[400px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-        blur-[100px]   "
+          className="absolute inset-0 bg-land-crimson/10 rounded-[9999px] w-[800px] h-[400px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+        blur-[200px]   "
         />
 
         {/* Title CARD: MEMBERSHIP RATES */}
@@ -21,309 +76,69 @@ export default function Membership() {
       </div>
 
       {/* GRID FOR DIFFERENT MEMBER PRICES */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[40px] lg:gap-[21px] mx-[32px] py-[33px] justify-items-center mt-16 mb-[96px] items-center   ">
-        {/* CONTAINER 1 */}
-        <div
-          className="group relative w-[320px] h-[366px] bg-land-card-hover ring-1 ring-land-border pl-[33px] justify-items-start rounded-[16px]
-       "
-        >
-          {/* type of pass */}
-          <div className="landing-p-lg font-medium  mt-[33px]">Day Pass</div>
-
-          {/* price */}
-          <div className="landing-h2 flex items-baseline mt-4">
-            $15
-            <div className="landing-p-sm text-muted">/ day </div>
-          </div>
-
-          {/* description */}
-          <div className="landing-p-sm text-muted  mt-4">
-            Perfect for travelers or trying us out.
-          </div>
-
-          {/* BENEFITS CONTAINER LIST*/}
-          <div className="landing-p-lg mt-[32px] gap-4 flex flex-col">
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">
-                Full gym access
-              </p>
-            </div>
-
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">
-                Locker room usage
-              </p>
-            </div>
-
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">
-                Valid for 24 hours
-              </p>
-            </div>
-          </div>
-        </div>
-
-        {/* CONTAINER 2 */}
-        <div
-          className="group relative w-[320px] h-[366px] bg-land-card-hover ring-1 ring-land-border pl-[33px] justify-items-start rounded-[16px]
-      "
-        >
-          {/* type of pass */}
-          <div className="landing-p-lg font-medium  mt-[33px]">
-            Student Rate
-          </div>
-
-          {/* price */}
-          <div className="landing-h2 flex items-center mt-4">
-            $35
-            <div className="landing-p-sm text-muted flex items-center ml-1">
-              /<span className="translate-y-[12px] ">month</span>
-            </div>
-          </div>
-
-          {/* description */}
-          <div className="landing-p-sm text-muted  mt-4">
-            Valid student ID require upon entry.
-          </div>
-
-          {/* BENEFITS CONTAINER LIST*/}
-          <div className="landing-p-lg mt-[32px] gap-4 flex flex-col">
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">
-                24/7 QR Access
-              </p>
-            </div>
-
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">
-                App Progress Tracking
-              </p>
-            </div>
-
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">
-                Flexible freeze policy
-              </p>
-            </div>
-          </div>
-        </div>
-
-        {/* CONTAINER 3 */}
-        <div
-          className="group relative w-[320px] h-[366px] bg-land-card-hover ring-1 ring-land-border pl-[33px] justify-items-start rounded-[16px]
-       "
-        >
-          {/* type of pass */}
-          <div className="landing-p-lg font-medium  mt-[33px]">Day Pass</div>
-
-          {/* price */}
-          <div className="landing-h2 flex items-baseline mt-4">
-            $15
-            <div className="landing-p-sm text-muted">/ day </div>
-          </div>
-
-          {/* description */}
-          <div className="landing-p-sm text-muted  mt-4">
-            Perfect for travelers or trying us out.
-          </div>
-
-          {/* BENEFITS CONTAINER LIST*/}
-          <div className="landing-p-lg mt-[32px] gap-4 flex flex-col">
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">
-                Full gym access
-              </p>
-            </div>
-
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">
-                Locker room usage
-              </p>
-            </div>
-
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">
-                Valid for 24 hours
-              </p>
-            </div>
-          </div>
-        </div>
-
-        {/* CONTAINER 4 */}
-        <div
-          className="group relative w-[336px] h-[387px] bg-land-card-hover ring-1 ring-land-crimson px-[33px] justify-items-start rounded-[16px]
-      "
-        >
-          {/* transition-all duration-327 ease-out hover:scale-107 hover:ring-land-crimson hover:shadow-[0_0_30px_rgba(214,0,0,0.3)] hover:z-30 cursor-pointer */}
-          {/* TITLE CARD FOR MOST POPULAR */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[40px] lg:gap-[15px] mx-[32px] py-[33px] justify-items-center mt-16 mb-[96px] items-center   ">
+        
+        
+        {/* INDIVIDUAL CARD FOR EACH MEMBERSHIP PLAN */}
+        {MEMBERSHIP_PLANS.map((plan) => (
           <div
-            className=" absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 
-                  bg-land-crimson text-main text-center tracking-[0.3px] text-[12px] font-bold
-                  px-[12px] py-[4px] rounded-[9999px] z-20 shadow-lg w-[121.2px] h-[25.2px] landing-p-sm  flex items-center justify-center"
+            key={plan.id}
+            className={`group relative bg-land-card-hover ring-1 justify-items-start rounded-[16px] ${
+              plan.isPopular
+                ? "w-[336px] h-[387px] ring-land-crimson px-[33px]"
+                : "w-[320px] h-[366px] ring-land-border pl-[33px]"
+            }`}
           >
-            MOST POPULAR
-          </div>
+            {/* POPULAR BADGE */}
+            {plan.isPopular && (
+              <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-land-crimson text-main text-center tracking-[0.3px] text-[12px] font-bold px-[12px] py-[4px] rounded-[9999px] z-20 shadow-lg w-[121.2px] h-[25.2px] landing-p-sm flex items-center justify-center">
+                MOST POPULAR
+              </div>
+            )}
 
-          {/* type of pass */}
-          <div className="landing-p-lg font-medium  mt-[33px]">
-            Monthly Regular
-          </div>
-
-          {/* price */}
-          <div className="landing-h2 flex items-center mt-4">
-            $50
-            <div className="landing-p-sm text-muted flex items-center ml-1">
-              /<span className="translate-y-[12px] ">month</span>
-            </div>
-          </div>
-
-          {/* description */}
-          <div className="landing-p-sm text-muted  mt-4">
-            Our standard membership for committed atheletes.
-          </div>
-
-          {/* BENEFITS CONTAINER LIST*/}
-          <div className="landing-p-lg mt-[32px] gap-4 flex flex-col">
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">
-                24/7 QR Access
-              </p>
+            {/* TYPE OF PASS */}
+            <div className="landing-p-lg font-medium mt-[33px]">
+              {plan.title}
             </div>
 
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">
-                1 Free Guest Pass/month
-              </p>
+            {/* PRICE */}
+            <div
+              className={`landing-h2 flex mt-4 ${plan.unit === "month" ? "items-center" : "items-baseline"}`}
+            >
+              ₱{plan.price}
+              <div
+                className={`landing-p-sm text-muted ${plan.unit === "month" ? "flex items-center ml-1" : "ml-1"}`}
+              >
+                /{" "}
+                <span
+                  className={plan.unit === "month" ? "translate-y-[12px]" : ""}
+                >
+                  {plan.unit}
+                </span>
+              </div>
             </div>
 
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">
-                App Progress Tracking
-              </p>
+            {/* DESCRIPTION */}
+            <div className="landing-p-sm text-muted mt-4">
+              {plan.description}
             </div>
 
-            {/* LIST ITEM */}
-            <div className="flex items-start gap-3">
-              {/* Check Mark Images */}
-              <img
-                src="/assets/redCheck.webp"
-                alt="bullet"
-                className="w-[16px] h-[16px] mt-1 object-cover"
-              />
-
-              {/* The list item text */}
-              <p className="pl-[12px] landing-p-sm text-muted">10% Off Merch</p>
-            </div>
+            {/* BENEFITS CONTAINER LIST */}
+            <ul className="landing-p-lg mt-[32px] gap-4 flex flex-col">
+              {plan.features.map((feature, index) => (
+                <li key={index} className="flex items-start gap-3">
+                  <img
+                    src="/assets/Checkmark.svg"
+                    alt=""
+                    aria-hidden="true"
+                    className="w-[16px] h-[16px] mt-1 object-cover"
+                  />
+                  <p className="pl-[12px] landing-p-sm text-muted">{feature}</p>
+                </li>
+              ))}
+            </ul>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );
