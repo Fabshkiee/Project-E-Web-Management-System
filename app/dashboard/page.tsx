ceffa8d9d5b96b426ec8f68c8125a95b5f0aee8c
Fabshkiee <pwedefabros@gmail.com>
2026-05-02 22:26:16 +0800

    feat: add total members card (hardcoded)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,31 @@
 import PageTitle from "@/components/dashboard/page-title";
+import StatsCard from "@/components/dashboard/overview-card";
+import { PeopleIcon } from "@/components/ui/Icons";
 
 export default function Dashboard() {
   return (
-    <PageTitle
-      title="Gym Overview"
-      subtitle={`Real-time statistics for ${new Date().toLocaleDateString(
-        "en-US",
-        {
-          weekday: "long",
-          month: "long",
-          day: "numeric",
-          year: "numeric",
-        },
-      )}`}
-    />
+    <div>
+      <PageTitle
+        title="Gym Overview"
+        subtitle={`Real-time statistics for ${new Date().toLocaleDateString(
+          "en-US",
+          {
+            weekday: "long",
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+          },
+        )}`}
+      />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-8">
+        <StatsCard
+          label="Total Members"
+          value="212"
+          icon={<PeopleIcon className="w-12 h-12" />}
+          trend={{ label: "12%", type: "up" }}
+        />
+      </div>
+    </div>
   );
 }
